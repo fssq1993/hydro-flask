2eb7ac88befc75b156811fd82e29380fac420738
yuanyi <yuanyi.yang@foxmail.com>
2018-07-17 15:27:23 -0400

    Add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -9,8 +9,22 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message) {
     console.log('New message', message)
+    const li = jQuery('<li></li>')
+    li.text(`${message.from}: ${message.text}`)
+    jQuery('#messages').append(li)
 })
 
 socket.on('createMessage', function(message) {
     console.log('Create message', message)
+})
+
+jQuery('#message-form').on('submit', function(e) {
+    e.preventDefault()
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    }, function() {
+
+    })
+
 })
