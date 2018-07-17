d0de8064ae88ed184554a4d6e48b7b9183c65a0d
yuanyi <yuanyi.yang@foxmail.com>
2018-07-17 18:37:56 -0400

    Send messages to only people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -75,7 +75,6 @@ const messageTextbox = jQuery('[name=message]')
 jQuery('#message-form').on('submit', function(e) {
     e.preventDefault()
     socket.emit('createMessage', {
-        from: 'User',
         text: messageTextbox.val()
     }, function() {
         messageTextbox.val('')
